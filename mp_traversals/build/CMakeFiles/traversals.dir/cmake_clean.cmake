1906ccbbaa50345765dd37e2219f8b84c39cc957
Anneqxy <113318686+Anneqxy@users.noreply.github.com>
2024-01-30 18:43:31 -0600

    Add files via upload

---
+++ mp_traversals/build/CMakeFiles/traversals.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/traversals.dir/entry/traversals.cpp.o"
+  "CMakeFiles/traversals.dir/entry/traversals.cpp.o.d"
+  "traversals"
+  "traversals.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/traversals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

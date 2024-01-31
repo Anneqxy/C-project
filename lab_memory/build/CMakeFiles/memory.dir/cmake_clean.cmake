819f2aad096a0719c868e5d1d3d82238b3e42779
Anneqxy <113318686+Anneqxy@users.noreply.github.com>
2024-01-30 18:31:45 -0600

    Add files via upload

---
+++ lab_memory/build/CMakeFiles/memory.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/memory.dir/entry/memory.cpp.o"
+  "CMakeFiles/memory.dir/entry/memory.cpp.o.d"
+  "memory"
+  "memory.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/memory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

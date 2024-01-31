1fc504bcc5f84272ea95594ca82346132969860b
Anneqxy <113318686+Anneqxy@users.noreply.github.com>
2024-01-30 18:33:33 -0600

    Add files via upload

---
+++ build/CMakeFiles/sketch.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sketch.dir/entry/sketch.cpp.o"
+  "CMakeFiles/sketch.dir/entry/sketch.cpp.o.d"
+  "sketch"
+  "sketch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sketch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
